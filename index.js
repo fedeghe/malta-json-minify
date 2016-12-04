ee4c6881d20e6dec54ed6b43c9a75079b51df756
Federico Ghedina <fedeghe@gmail.com>
2016-12-04 22:55:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var path = require('path'),
+	fs = require('fs');
+	
+function malta_json_uglify(o, options) {
+
+	var self = this,
+		start = new Date(),
+		msg = "",
+		pluginName = path.basename(path.dirname(__filename));
+
+	o.content = o.content
+		.replace(/\"\s*(:|,)\s*\"/g, '"$1"')
+		.replace(/,\n/g, ',')
+		.replace(/[\n|\t]/g, '');
+	
+	return function (solve, reject){
+		try {
+			fs.writeFile(o.name, o.content, function(err) {
+				err && self.doErr(err, o, pluginName);
+				msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
+				solve(o);
+				self.notifyAndUnlock(start, msg);
+			});	
+		} catch(err) {
+			self.doErr(err, o, pluginName);
+		}
+	};
+}
+malta_json_uglify.ext = ['json'];
+module.exports = malta_json_uglify;
